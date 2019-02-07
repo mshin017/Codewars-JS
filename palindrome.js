e6d004e775b04dd43b21d273836e07de22c386db
Michelle Shin <mshin017@ucr.edu>
2019-02-06 20:30:50 -0800

    strarr.js

--- palindrome.js
+++ palindrome.js
@@ -1,5 +1,45 @@
 // determine if string is palindrome
 
-string.split('').reverse().join('')
+//string.split('').reverse().join('')
+
+var foo = 10+'20'
+console.log(foo)
+
+console.log(0.1 + 0.2 == 0.3)
 
 
+str="i'm a lasagna hog"
+console.log(str.split("").reverse().join(""))
+
+function add(x) {
+    return function(y) {
+        return x + y;
+    };
+}
+console.log(add(2)(5))
+
+function addd(x,y){
+    return x+y
+}
+console.log(addd(2,5))
+
+// var foo = 'Hello';
+// (function(){
+//     var bar = 'World';
+//     console.log(foo+bar)
+// })();
+// console.log(foo+bar)
+
+var boo = [];
+boo.push(1);
+boo.push(2);
+// console.log(boo.length)
+
+console.log('one')
+setTimeout(function(){
+    console.log('two')
+},0)
+Promise.resolve().then(function(){
+    console.log('three')
+})
+console.log('four')
